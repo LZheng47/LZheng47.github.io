6bb78e814ececc0909c307e3408bafdb8a521d5a
Lily Zheng <57676919+LZheng47@users.noreply.github.com>
2021-02-10 02:05:40 -0500

    Add files via upload

--- mp3_resources/mp3.js
+++ mp3_resources/mp3.js
@@ -1,1 +1,18 @@
+function init(){
+    let navright = document.getElementById("navright");
+    let build = "";
 
+    for(let x = 1; x <= 4; x++){
+        build += `<a href="mp${x}.html">`
+        build += `  <h4> MARKING PD ${x} </h4>`
+        build += `</a>`
+    }
+    navright.innerHTML = build;
+    
+    //mp3 setup    
+    mpcomfortable(2, "mpas3", 2, 31, 3);
+    let mprows3_a = document.querySelectorAll(".mprows3 a");
+    for(let x = 0; x < mprows3_a.length; x++){
+        randompos(mprows3_a[x], 300, 800, 200, 600)
+    }
+}
