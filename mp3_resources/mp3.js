a5b0241cd495ca837a8fa0f1bac94dc5522cb325
Lily Zheng <57676919+LZheng47@users.noreply.github.com>
2021-02-21 18:10:59 -0500

    Add files via upload

--- mp3_resources/mp3.js
+++ mp3_resources/mp3.js
@@ -10,7 +10,7 @@ function init(){
     navright.innerHTML = build;
     
     //mp3 setup    
-    mpcomfortable(2, "mpas3", 2, 31, 3);
+    mpcomfortable(2, 3, 2, 31, 3);
     let mprows3_a = document.querySelectorAll(".mprows3 a");
     for(let x = 0; x < mprows3_a.length; x++){
         randompos(mprows3_a[x], 300, 800, 200, 600)
