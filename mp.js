fd439a30f90b664e7be1f52873afbfa485ab2d2a
Lily Zheng <57676919+LZheng47@users.noreply.github.com>
2021-02-21 17:35:44 -0500

    Add files via upload

--- mp.js
+++ mp.js
@@ -5,12 +5,10 @@ function mainhron(){
     mainhr.style.maxWidth = "600px";
 }
 
-
 function mainhroff(){
     let mainhr = document.getElementById("mainhr");
     mainhr.style.maxWidth = "560px";
 }
-
 
 function mpcomfortable(t, mpnum, ASamt, c, classnum){
     let mpas = document.getElementById(mpnum);
@@ -26,11 +24,13 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
 
     for(let x = 1; x <= ASamt/5; x++){
         build += `<div class="mprows mprows${classnum}">`
+        
         count++
-        build += `  <a href="files_viewer.html" data-pathway="WDT${t}Lesson${count}/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="WDT${t}Lesson${count}/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
 
         count++
         build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="WDT${t}Lesson${count}/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+        
         build += `</div>`
         build += `<div class="mprows mprows${classnum}">`
 
@@ -86,8 +86,21 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
             build += `</div>`
         }
     }
-
     mpas.innerHTML = build;
+
+    for(let x = 0; x < mpas.getElementsByTagName("div").length; x++){
+        for(let index = 0; index < mpas.getElementsByTagName("div")[x].getElementsByTagName("a").length; index++){
+            console.log(mpas.getElementsByTagName("div")[x].getElementsByTagName("a")[index].getAttribute("data-pathway"));
+        }
+    }
+}
+
+let index_path = "";
+
+function pathway(obj){
+    index_path = obj.getAttribute("data-pathway");
+    localStorage.setItem("index_path", index_path);
+    console.log(index_path);
 }
 
 function randompos(obj, x1, x2, y1, y2){
