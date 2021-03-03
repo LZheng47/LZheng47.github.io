c579cefcf50197215dd36022b812cf48f0a06a52
Lily Zheng <57676919+LZheng47@users.noreply.github.com>
2021-03-02 19:00:26 -0500

    Add files via upload

--- mp.js
+++ mp.js
@@ -12,8 +12,20 @@ function mainhroff(){
 
 function mpcomfortable(t, mpnum, ASamt, c, classnum){
     let mpas = document.getElementById(`mpas${mpnum}`);
+    let body = document.getElementsByTagName("body")[0];
     let build = "";
+    let ref = "";
     let ASamtExtra = 0;
+
+    if(mpnum == 3 || mpnum == 4){
+        ref = "files_viewer.html";
+    }else{
+        ref = '';
+    }
+
+    body.setAttribute("data-mp", mpnum);
+    mp = body.getAttribute("data-mp");
+    localStorage.setItem("mp", mp);
 
     while(ASamt % 5 > 0){
         ASamt -= 1;
@@ -21,29 +33,29 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
     }
 
     let count = c-1;
-
+    
     for(let x = 1; x <= ASamt/5; x++){
         build += `<div class="mprows mprows${classnum}">`
         
         count++
-        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+        build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
 
         count++
-        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+        build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:2"> T${t}_Lesson${count} </a>`
         
         build += `</div>`
         build += `<div class="mprows mprows${classnum}">`
 
         count++
-        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+        build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:2"> T${t}_Lesson${count} </a>`
 
         count++
-        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+        build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
         build += `</div>`
         build += `<div class="mprows mprows${classnum}">`
 
         count++
-        build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+        build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
         build += `</div>`
     }
 
@@ -51,18 +63,18 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
         for(let x = 0; x < 1; x++){
             build += `<div class="mprows mprows${classnum}">`
             count++
-            build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+            build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
             count++
-            build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+            build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:2"> T${t}_Lesson${count} </a>`
             build += `</div>`
         }
         if(ASamtExtra == 4){
             for(let x = 0; x < 1; x++){
                 build += `<div class="mprows mprows${classnum}">`
                 count++
-                build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+                build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:2"> T${t}_Lesson${count} </a>`
                 count++
-                build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+                build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
                 build += `</div>`
             }
         }
@@ -72,15 +84,15 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
         for(let x = 0; x < 1; x++){
             build += `<div class="mprows mprows${classnum}">`
             count++
-            build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+            build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
 
             if(ASamtExtra == 3){
                 count++
-                build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:2"> T${t}_Lesson${count} </a>`
+                build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:2"> T${t}_Lesson${count} </a>`
                 build += `</div>`
                 build += `<div class="mprows mprows${classnum}">`
                 count++
-                build += `  <a href="files_viewer.html" onclick="pathway(this)" data-pathway="mp${mpnum}_resources/WDT${t}Lesson${count}_1/index.html" style="flex:1"> T${t}_Lesson${count} </a>`
+                build += `  <a href="${ref}" onclick="pathway(this)" data-pathway="${count}" style="flex:1"> T${t}_Lesson${count} </a>`
             }
 
             build += `</div>`
@@ -88,11 +100,12 @@ function mpcomfortable(t, mpnum, ASamt, c, classnum){
     }
     mpas.innerHTML = build;
 
+    /*
     for(let x = 0; x < mpas.getElementsByTagName("div").length; x++){
         for(let index = 0; index < mpas.getElementsByTagName("div")[x].getElementsByTagName("a").length; index++){
             console.log(mpas.getElementsByTagName("div")[x].getElementsByTagName("a")[index].getAttribute("data-pathway"));
         }
-    }
+    }*/
 }
 
 let index_path = "";
@@ -100,7 +113,6 @@ let index_path = "";
 function pathway(obj){
     index_path = obj.getAttribute("data-pathway");
     localStorage.setItem("index_path", index_path);
-    console.log(index_path);
 }
 
 function randompos(obj, x1, x2, y1, y2){
